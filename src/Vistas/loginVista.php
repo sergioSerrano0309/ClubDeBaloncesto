adf55c8cddee3196ed74e4147f80c5b622ab0d65
sergioSerrano0309 <103897663+sergioSerrano0309@users.noreply.github.com>
2023-04-18 15:22:28 +0200

    correigo titulo del modal y nombre de la configuracion (tema oscuro -> tema)

--- src/Vistas/loginVista.php
+++ src/Vistas/loginVista.php
@@ -60,21 +60,21 @@ if ($_SERVER["REQUEST_METHOD"]=="POST")
 
 
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p style="display: inline;">Tema Oscuro: <button style="display: inline;" id="tema" oscuro="false" class="tema" onclick=modoOscuro()><i class="fa-solid fa-sun"></i></button></p> 
-                </div>
-                <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
+        <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">Ajustes</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <div class="modal-body">
+                <p style="display: inline;">Tema : <button style="display: inline;" id="tema" oscuro="false" class="tema" ><i class="fa-solid fa-sun"></i></button></p> 
+            </div>
+            <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
+        </div>
+    </div>
     </div>
 </body>
 </html>
