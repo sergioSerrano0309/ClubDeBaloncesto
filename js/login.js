7a2f45ee1a42c889a891e5e5cfd5ba4db59965a1
sergioSerrano0309 <103897663+sergioSerrano0309@users.noreply.github.com>
2023-04-18 15:23:01 +0200

    intento de inserción de una cookie

--- js/login.js
+++ js/login.js
@@ -1,3 +1,6 @@
+window.onload = function(){
+    document.getElementById("tema").addEventListener("click", modoOscuro);
+}
 function modoOscuro() {
     var body = document.body;
     var usuario = document.getElementById("usuario");
@@ -7,6 +10,7 @@ function modoOscuro() {
     var form = document.getElementById("form");
 
     if(oscuro == "false"){
+        document.cookie = "tema=oscuro; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
         body.style.backgroundColor = "#002b47";
         botonTema.setAttribute("oscuro", "true");
         botonTema.innerHTML = '<i class="fa-solid fa-moon"></i>';
@@ -19,6 +23,7 @@ function modoOscuro() {
         form.style.backgroundColor = "rgba(23, 57, 97, 0.95)";
 
     } else if(oscuro == "true"){
+        document.cookie = "tema=claro; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
         body.style.backgroundColor = "#c4e0ff";
         botonTema.setAttribute("oscuro", "false");
         botonTema.innerHTML = '<i class="fa-solid fa-sun"></i>';
