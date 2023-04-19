d7e841e46a49ad9e6ae340786c2b42c3af7b1457
sergioSerrano0309 <103897663+sergioSerrano0309@users.noreply.github.com>
2023-04-19 13:01:59 +0200

    añadido para que el menu de ajustes tambien cambie a oscuro y añadido función para cambiar de idioma

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 window.onload = function(){
     document.getElementById("tema").addEventListener("click", modoOscuro);
+    document.getElementById("idioma").addEventListener("change", cambiarIdioma);
 }
 function modoOscuro() {
     var body = document.body;
@@ -8,7 +9,7 @@ function modoOscuro() {
     var botonTema  = document.getElementById("tema");
     var oscuro = botonTema.getAttribute("oscuro");
     var form = document.getElementById("form");
-
+    var modal = document.getElementById("exampleModal");
     if(oscuro == "false"){
         document.cookie = "tema=oscuro; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
         body.style.backgroundColor = "#002b47";
@@ -21,6 +22,8 @@ function modoOscuro() {
         contraseña.style.borderColor = "#002b47";
         contraseña.style.color = "white";
         form.style.backgroundColor = "rgba(23, 57, 97, 0.95)";
+        modal.setAttribute("data-bs-theme","dark");
+        modal.style.color="white";
 
     } else if(oscuro == "true"){
         document.cookie = "tema=claro; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
@@ -34,6 +37,46 @@ function modoOscuro() {
         contraseña.style.borderColor = "#ffff";
         contraseña.style.color = "black";
         form.style.backgroundColor = "rgba(65, 154, 255, 0.95)";
+        modal.setAttribute("data-bs-theme","light");
+        modal.style.color="black";
     }
     
 }
+
+function cambiarIdioma() {
+    var idiomaSeleccionado = document.getElementById("idioma").value;
+    if (idiomaSeleccionado === "es") {
+      document.getElementById("user").innerHTML = "Nombre de usuario";
+      document.getElementById("password").innerHTML = "Contraseña";
+      document.getElementById("confirmar").value = "Confirmar";
+      document.getElementById("ajustes").innerHTML = "Ajustes";
+      document.getElementById("close").innerHTML = "Cerrar"; 
+      document.getElementById("setting1").innerHTML = "Tema:";
+      document.getElementById("setting2").innerHTML = "<br>Lenguaje:";
+      document.getElementById("setting21").innerHTML = "Castellano";
+      document.getElementById("setting22").innerHTML = "Inglés";
+      document.getElementById("setting23").innerHTML = "Catalán"; 
+    } else if (idiomaSeleccionado === "en") {
+        document.getElementById("user").innerHTML = "Username";
+        document.getElementById("password").innerHTML = "Password";
+        document.getElementById("confirmar").value = "Confirm";
+        document.getElementById("ajustes").innerHTML = "Settings";
+        document.getElementById("close").innerHTML = "Close";
+        document.getElementById("setting1").innerHTML = "Theme:";
+        document.getElementById("setting2").innerHTML = "<br>Language:";
+        document.getElementById("setting21").innerHTML = "Spanish";
+        document.getElementById("setting22").innerHTML = "English";
+        document.getElementById("setting23").innerHTML = "Catalan"; 
+    } else if (idiomaSeleccionado === "ca") {
+        document.getElementById("user").innerHTML = "Nom d'usuari";
+        document.getElementById("password").innerHTML = "Contrasenya";
+        document.getElementById("confirmar").value = "Confirmar";
+        document.getElementById("ajustes").innerHTML = "Ajustaments";
+        document.getElementById("close").innerHTML = "Tancar";
+        document.getElementById("setting1").innerHTML = "Tema:";
+        document.getElementById("setting2").innerHTML = "<br>Llenguatje:";
+        document.getElementById("setting21").innerHTML = "Castellà";
+        document.getElementById("setting22").innerHTML = "Anglès";
+        document.getElementById("setting23").innerHTML = "Català"; 
+    }
+  }
